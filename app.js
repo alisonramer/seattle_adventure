3bd0a2411b38ffd7892c1bc350ac7cf4e68b4bf6
Attonasi <attonasi@hotmail.com>
2016-12-19 11:57:27 -0800

    added local storage function

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 'use strict';
-
+// ******************************************* storing username for local storage ***************
 var nameForm = document.getElementById('nameForm');
 
 function setName(event){
@@ -8,8 +8,20 @@ function setName(event){
 
   var nameInput = document.getElementById('nameInput');
   var userName = event.target.nameInput.value;
+
+  sendToLocalStorage(userName);
+
   console.log(userName, nameInput);
 
 }
-
+// ***************************************** event listener for username *********************
 nameForm.addEventListener('submit', setName);
+// ******************************************************************************************
+
+function sendToLocalStorage(userName){
+  var stringifiedName = JSON.stringify(userName);
+  localStorage.setItem(userName, stringifiedName);
+
+  console.log(stringifiedName);
+
+}
