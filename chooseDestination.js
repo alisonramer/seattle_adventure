8ea5a8865c25039e8e6ea7e2a17c997827bec2c0
Attonasi <attonasi@hotmail.com>
2016-12-20 14:15:03 -0800

    argle bargle

--- chooseDestination.js
+++ chooseDestination.js
@@ -8,7 +8,7 @@ var stringifiedName = localStorage.getItem('currentUser');
 var userName = JSON.parse(stringifiedName);
 var stringifiedArray = localStorage.getItem('story');
 var storyArray = JSON.parse(stringifiedArray);
-var destinationArray = ['seattleCenter.html', 'PikePlace/pikePlace.html', 'aquarium/aquarium.html' ];
+var destinationArray = ['seattleCenter/seattleCenter.html', 'PikePlace/pikePlace.html', 'aquarium/aquarium.html' ];
 var imageArray = ['img/Seattle_Center.jpg', 'img/Pike_Place.jpg', 'img/aquarium.jpg']
 var destinationChoice;
 var left = document.getElementById('left');
