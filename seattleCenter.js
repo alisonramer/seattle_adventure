c4f64653e89eab2b44c0e39154ee6a27a0d19853
Attonasi <attonasi@hotmail.com>
2016-12-20 13:29:14 -0800

    css/html globalisation

--- seattleCenter.js
+++ seattleCenter.js
@@ -8,3 +8,5 @@ var storyArray = JSON.parse(stringifiedArray);
 var seattleCenterArray = ['Space Needle', 'Fountain', 'Flowers'];
 
 console.log(chooseDestinationBox, userName, storyArray, seattleCenterArray);
+
+document.getElementById('headerTextElement').textContent = userName+' has run to the Seattle Center! Where do you want to go?';
