f184374799f5549b3a540914a37c83af73f8b362
Attonasi <attonasi@hotmail.com>
2016-12-19 22:49:26 -0800

    glorious night of creating problems

--- chooseDestination.js
+++ chooseDestination.js
@@ -1,11 +1,43 @@
 'use strict';
 
-var chooseDestinationBox = document.getElementById('avatarBox');
+var chooseDestinationBox = document.getElementById('chooseDestinationBox');
 var stringifiedName = localStorage.getItem('currentUser');
 var userName = JSON.parse(stringifiedName);
-// console.log(stringifiedName, userName);
-
 var stringifiedArray = localStorage.getItem('story');
 var storyArray = JSON.parse(stringifiedArray);
+var destinationArray = ['seattleCenter.html', 'pikePlace.html'];
 
-console.log(storyArray, userName, chooseDestinationBox);
+// console.log(storyArray, userName, chooseDestinationBox);
+
+// FUNCTIONS >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+function sendToLocalStorage(userKey, something){
+  var stringifiedName = JSON.stringify(something);
+  // var currentUserKey = JSON.stringify(userKey);
+  localStorage.setItem(userKey, stringifiedName);
+
+}
+
+// EVENT HANDLERS>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+function setDestination(event){
+  event.preventDefault();
+  for (var i = 0; i < storyArray.length; i++) {
+
+    if(storyArray[i].name === userName){
+      for (var o = -1; o < storyArray[i].placesGone.length; o++) {
+
+        if(storyArray[i].placesGone.includes(event.target.alt) === false){
+          storyArray[i].placesGone.push(event.target.alt);
+        }
+      }
+    }
+  }
+  sendToLocalStorage('story', storyArray);
+  // console.log(destinationArray[event.target.alt]);
+  window.location.href = destinationArray[event.target.alt];
+}
+
+// EVENT LISTENERS >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+chooseDestinationBox.addEventListener('click', setDestination);
