12b4ea91eb0a07090c96789b6552b17cdcf48517
Attonasi <attonasi@hotmail.com>
2016-12-21 09:47:03 -0800

    alison and gary might have messed m up

---
+++ end.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var stringifiedName = localStorage.getItem('currentUser');
+var userName = JSON.parse(stringifiedName);
+var stringifiedArray = localStorage.getItem('story');
+var storyArray = JSON.parse(stringifiedArray);
+var imageArray = ['img/Seattle_Center.jpg', 'img/Pike_Place.jpg', 'img/aquarium.jpg']
+
+var left = document.getElementById('left');
+var right = document.getElementById('right');
+var center = document.getElementById('center');
+
+for (var i = 0; i < storyArray.length; i++) {
+
+  if(storyArray[i].name===userName){
+    document.getElementById('avatarImg').src = 'img/avatars/' +storyArray[i].avatar +'.png';
+    var currentAvatar = storyArray[i].avatar;
+    var placesGone = storyArray[i].placesGone;
+  }
+}
+
+left.src = imageArray[placesGone[0]];
+center.src = imageArray[placesGone[1]];
+right. src = imageArray[placesGone[2]];
