8dd4544a0b0e5a8d66d174a01de3fbf526eb724d
Attonasi <attonasi@hotmail.com>
2016-12-21 16:19:04 -0800

    did great things for journal

--- journal.js
+++ journal.js
@@ -4,12 +4,16 @@ var stringifiedName = localStorage.getItem('currentUser');
 var userName = JSON.parse(stringifiedName);
 var stringifiedArray = localStorage.getItem('story');
 var storyArray = JSON.parse(stringifiedArray);
-var imageArray = ['img/Seattle_Center.jpg', 'img/Pike_Place.jpg', 'img/aquarium.jpg'];
+var imageArray = ['img/Seattle_Center.jpg', 'img/Pike_Place.jpg', 'img/aquarium.jpg', 'img/gasworks/gasworks.jpg', 'img/alki/beach.jpg', 'img/locks/locks.jpg', 'img/lincoln/foresttrail.jpg', 'img/lakeunion/mohai.jpg']
+var placeNamesArray = ['Seattle Center', 'Pike Place', 'The Aquarium', 'Gasworks', 'Alki Beach', 'The Locks', 'Lincoln Park', 'Lake Union'];
 
+var headerId = document.getElementById('headerId');
+var firstPlaceText = document.getElementById('firstPlaceText');
+var secondPlaceText = document.getElementById('secondPlaceText');
+var thirdPlaceText = document.getElementById('thirdPlaceText');
 var firstPlace = document.getElementById('firstPlace');
 var secondPlace = document.getElementById('secondPlace');
 var thirdPlace = document.getElementById('thirdPlace');
-
 
 for (var i = 0; i < storyArray.length; i++) {
 
@@ -19,6 +23,11 @@ for (var i = 0; i < storyArray.length; i++) {
     var placesGone = storyArray[i].placesGone;
   }
 }
+
+headerId.textContent = userName+' the '+currentAvatar+' has been to many places today!'
+firstPlaceText.textContent = 'First '+userName+' went to ' +placeNamesArray[placesGone[0]]+'!';
+secondPlaceText.textContent = 'Then '+userName+' went to ' +placeNamesArray[placesGone[1]]+'!';
+thirdPlaceText.textContent = 'Finally ' +userName+' went to ' +placeNamesArray[placesGone[2]]+'!';
 
 console.log(userName, currentAvatar, placesGone);
 
