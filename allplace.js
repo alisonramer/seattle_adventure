7aa514cc0252434eb4d0bc620e998f2d11c6ee3c
Attonasi <attonasi@hotmail.com>
2016-12-21 17:51:46 -0800

    going home

--- allplace.js
+++ allplace.js
@@ -5,15 +5,20 @@ var stringifiedName = localStorage.getItem('currentUser');
 var userName = JSON.parse(stringifiedName);
 var stringifiedArray = localStorage.getItem('story');
 var storyArray = JSON.parse(stringifiedArray);
+var headerId = document.getElementById('Welcome');
+var placeNamesArray = ['Seattle Center', 'Pike Place', 'The Aquarium', 'Gasworks', 'Alki Beach', 'The Locks', 'Lincoln Park', 'Lake Union'];
 
 for (var i = 0; i < storyArray.length; i++) {
 
   if(storyArray[i].name===userName){
     document.getElementById('avatarImg').src = '../img/avatars/' +storyArray[i].avatar +'.png';
     var currentAvatar = storyArray[i].avatar;
+    var indexNumber = i;
     // storyArray[i].placesGone.push('2');
   }
 }
+console.log(indexNumber, placeNamesArray[indexNumber]);
+headerId.textContent = currentAvatar+' has gone to ' +placeNamesArray[indexNumber];
 
 function backToChoose(event) {
   event.preventDefault();
