615ad50fcd3289f9be62c4ce406da549a4af54b7
Gary Lundgren <builtbygl@gmail.com>
2016-12-22 14:15:48 -0800

    text

---
+++ aquarium/aquarium.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var backButton = document.getElementById('backButton');
+var stringifiedName = localStorage.getItem('currentUser');
+var userName = JSON.parse(stringifiedName);
+var stringifiedArray = localStorage.getItem('story');
+var storyArray = JSON.parse(stringifiedArray);
+
+for (var i = 0; i < storyArray.length; i++) {
+
+  if(storyArray[i].name===userName){
+    document.getElementById('avatarImg').src = '../img/avatars/' +storyArray[i].avatar +'.png';
+    var currentAvatar = storyArray[i].avatar;
+    // storyArray[i].placesGone.push('2');
+  }
+}
+
+function backToChoose(event) {
+  event.preventDefault();
+
+
+  sendToLocalStorage('story', storyArray);
+  window.location.href = '../chooseDestination.html';
+}
+
+function sendToLocalStorage(userKey, something){
+  var stringifiedName = JSON.stringify(something);
+  localStorage.setItem(userKey, stringifiedName);
+}
+
+backButton.addEventListener('click', backToChoose);
